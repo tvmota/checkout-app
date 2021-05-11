b08158f57c01529b16eb24732710547af6477559
tiago vieira mota <tiago.vieira.mota@gmail.com>
2021-05-11 12:39:01 -0300

    setup inicial do projeto

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import TailwindCSS from './TailwindCSS.svelte';
+  import logo from './assets/logo.svg';
+</script>
+
+<TailwindCSS />
+<h1>Hello Svelte!</h1>
+<img class="w-64 h-64" src={logo} alt="Svelte Logo" />
---
+++ src/TailwindCSS.svelte
@@ -0,0 +1,8 @@
+<style global>
+  @tailwind base;
+  body {
+    @apply font-sans bg-indigo-200;
+  }
+  @tailwind components;
+  @tailwind utilities;
+</style>
